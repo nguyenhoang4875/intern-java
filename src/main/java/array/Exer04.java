ba07a5600c194855781c0aaf0f3c0becf78c0abe
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-30 10:21:12 +0700

    exercise-04 optimize v1

--- src/main/java/array/Exer04.java
+++ src/main/java/array/Exer04.java
@@ -7,29 +7,9 @@ public class Exer04 {
         for (int i = 0; i < array.length - 1; i++) {
 
             for (int j = i + 1; j < array.length; j++) {
-                if ((array[i] % 2 != 0) && (array[j] % 2 == 0)) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
-
-        for (int i = 0; array[i] % 2 == 0; i++) {
-
-            for (int j = i + 1; array[j] % 2 == 0; j++) {
-                if (array[i] > array[j]) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
-
-        for (int i = array.length - 1; array[i] % 2 != 0; i--) {
-
-            for (int j = i - 1; array[j] % 2 != 0; j--) {
-                if (array[i] > array[j]) {
+                if (((array[i] % 2 != 0) && (array[j] % 2 == 0)) ||
+                        (((array[i] % 2 == 0) && (array[j] % 2 == 0)) && (array[i] > array[j])) ||
+                        ((array[i] % 2 != 0) && (array[j] % 2 != 0)) && (array[i] < array[j])) {
                     int temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
