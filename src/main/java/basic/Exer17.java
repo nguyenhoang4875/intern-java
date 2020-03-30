6a300ef9227edecd2524e9fc204fc65f37d7e327
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-30 10:35:51 +0700

    exercise-17 optimize v1.0

--- src/main/java/basic/Exer17.java
+++ src/main/java/basic/Exer17.java
@@ -8,14 +8,11 @@ public class Exer17 {
             return 0;
         }
         double start = 0;
-        double end = n;
-        if (n < 1) {
-            end = 1;
-        }
+        double end = n < 1 ? 1 : n;
         double middle = (start + end) / 2;
 
-        while (Math.abs(n - middle * middle) > epsilon) {
-            if ((n - start * start) * (n - middle * middle) < 0) {
+        while (end - middle > epsilon) {
+            if (middle * middle > n) {
                 end = middle;
             } else {
                 start = middle;
