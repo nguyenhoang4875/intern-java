74a4550e9b031775caa995e17e741b80942143aa
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-30 11:05:52 +0700

    exercise-06 optimize v1.0

--- src/main/java/array/Exer06.java
+++ src/main/java/array/Exer06.java
@@ -8,11 +8,12 @@ public class Exer06 {
         for (int i = 0; i < array.length - 1; i++) {
 
             for (int j = i + 1; j < array.length; j++) {
-                if ( (((array[i] % 2 == 0) && (array[j] % 2 == 0)) && (array[i] > array[j])) ||
-                        ((array[i] % 2 != 0) && (array[j] % 2 != 0)) && (array[i] < array[j])) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+                if ((array[i] + array[j]) % 2 == 0) {
+                    if ((array[i] % 2 == 0) == (array[i] > array[j])) {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
                 }
             }
         }
