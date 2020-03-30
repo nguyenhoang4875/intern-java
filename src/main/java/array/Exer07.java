d0ce37fc0c0d7bd731c397fae441cace3c145c0a
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-30 14:33:32 +0700

    exercise-07 v2

--- src/main/java/array/Exer07.java
+++ src/main/java/array/Exer07.java
@@ -5,20 +5,16 @@ import java.util.List;
 
 public class Exer07 {
 
-    public static void main(String[] args) {
-        new Exer07().findLongestArrayNotFalling(new int[]{0, 1, 2, 1, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 0, 1, 2, 3, 4, 5, 0});
-    }
-
     public List findLongestArrayNotFalling(int[] array) {
 
-        int[][] arrayResult = new int[array.length][2];
         List<Integer> listResult = new ArrayList<>();
-        int count = 0;
         if (array.length == 1) {
             listResult.add(array[0]);
             return listResult;
         }
 
+        int[][] arrayResult = new int[array.length][2];
+        int count = 0;
         for (int i = 0; i < array.length - 1; i++) {
 
             for (int j = i + 1; j < array.length; j++) {
@@ -32,11 +28,6 @@ public class Exer07 {
                     }
                     continue;
                 } else {
-                    if (j == (array.length - 1)) {
-                        arrayResult[count][0] = 1;
-                        arrayResult[count][1] = j;
-                        count++;
-                    }
                     arrayResult[count][0] = j - i;
                     arrayResult[count][1] = i;
                     i = j - 1;
@@ -54,7 +45,6 @@ public class Exer07 {
                 index = i;
             }
         }
-        StringBuilder result = new StringBuilder();
 
         for (int i = arrayResult[index][1]; i < arrayResult[index][1] + arrayResult[index][0]; i++) {
             listResult.add(array[i]);
--- src/test/java/array/Exer07Test.java
+++ src/test/java/array/Exer07Test.java
@@ -9,6 +9,7 @@ public class Exer07Test {
 
     @Test
     public void test_findLongestArrayNotFalling() {
+        Assert.assertEquals(new Exer07().findLongestArrayNotFalling(new int[]{1}), Arrays.asList(1));
         Assert.assertEquals(new Exer07().findLongestArrayNotFalling(new int[]{1, 0, -1}), Arrays.asList(1));
         Assert.assertEquals(new Exer07().findLongestArrayNotFalling(new int[]{1, 2, 3, 2, 3, 4, 5, 0, 4, 5, 6}), Arrays.asList(2, 3, 4, 5));
         Assert.assertEquals(new Exer07().findLongestArrayNotFalling(new int[]{1, 2, 3, 6, 0, 3, 4, 5, 7, 8, 2, 1, 2, 3, 4, 5, 6, 7}), Arrays.asList(1, 2, 3, 4, 5, 6, 7));
