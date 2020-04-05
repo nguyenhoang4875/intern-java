d58f45619d320d142d4d486e0078aa573c081fdd
Võ Quang Hòa <voquanghoa@gmail.com>
2020-04-05 10:14:20 +0700

    Add fail

--- src/test/java/string/Exer11Test.java
+++ src/test/java/string/Exer11Test.java
@@ -7,7 +7,14 @@ public class Exer11Test {
 
     @Test
     public void test_sumNumbers(){
-        Assert.assertEquals(new Exer11().subNumbers("2","1"),1);
-        Assert.assertEquals(new Exer11().subNumbers("6912","5678"),1234);
+        Assert.assertEquals(new Exer11().subNumbers(
+                "137817381738163715371357189182918298129829182981",
+                "8178217281786173715325362"),
+                "137817381738163715371349010965636511956113857619");
+
+        Assert.assertEquals(new Exer11().subNumbers(
+                "137817381738163715371349010965636511956113857619",
+                "182781271837192381617467246724527542637263263823"),
+                "-44963890099028666246118235758891030681149406204");
     }
 }
