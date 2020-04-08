134835ead66d9ac7602d3ce4ac4e8a0c0302ca9f
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-04-08 17:49:26 +0700

    exercise-04 v3

--- src/main/java/oop/Stack.java
+++ src/main/java/oop/Stack.java
@@ -1,3 +1,4 @@
+
 package oop;
 
 public class Stack {
@@ -8,49 +9,24 @@ public class Stack {
         stack = new Object[100];
     }
 
-    public Stack(Object[] array) {
-        stack = new Object[100];
-        count = array.length - 1;
-        clone(array, stack);
-    }
-
-    public Object push(int item) {
+    public Object push(Object item) {
         stack[++count] = item;
         return item;
     }
 
     public Object pop() {
-        Object temp = stack[count];
-        stack[count] = 0;
-        return temp;
+        return stack[count--];
     }
 
     public Object peek() {
         return stack[count];
     }
 
-    public Object search(Object item) {
-
-        for (int i = 0; i <= count; i++) {
-            if (item == stack[i]) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     public boolean empty() {
         return count == 0;
     }
 
-    public Object size() {
+    public int size() {
         return count == 0 ? 0 : ++count;
-    }
-
-    public void clone(Object[] src, Object[] des) {
-
-        for (int i = 0; i < src.length; i++) {
-            des[i] = src[i];
-        }
     }
 }
