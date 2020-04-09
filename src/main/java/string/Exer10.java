c681b8b7bb5ecaa80efc0202128a61f92624e180
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-04-09 11:45:20 +0700

    exercise-10 v2

--- src/main/java/string/Exer10.java
+++ src/main/java/string/Exer10.java
@@ -2,15 +2,39 @@ package string;
 
 public class Exer10 {
 
-    public int sumNumbers(String num1, String num2) {
-        return getNumber(num1) + getNumber(num2);
-    }
-
-    public int getNumber(String number) {
-        int result = 0;
-        for (int i = 0; i < number.length(); i++) {
-            result = result * 10 + number.charAt(i) - '0';
+    public String sumNumbers(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+        int lenStr1 = num1.length();
+        int lenStr2 = num2.length();
+        String maxNumLen;
+        String minNumLen;
+        if (lenStr1 >= lenStr2) {
+            maxNumLen = new StringBuilder(num1).reverse().toString();
+            minNumLen = new StringBuilder(num2).reverse().toString();
+        } else {
+            maxNumLen = new StringBuilder(num2).reverse().toString();
+            minNumLen = new StringBuilder(num1).reverse().toString();
         }
-        return result;
+        int temp;
+        int carry = 0;
+
+        for (int i = 0; i < minNumLen.length(); i++) {
+            temp = maxNumLen.charAt(i) - '0' + minNumLen.charAt(i) - '0';
+            result.append((temp % 10 + carry) % 10);
+            carry = (temp + carry) / 10;
+        }
+        if (maxNumLen.length() > minNumLen.length()) {
+
+            for (int i = minNumLen.length(); i < maxNumLen.length(); i++) {
+                if (carry != 0) {
+                    temp = maxNumLen.charAt(i) - '0' + carry;
+                    result.append(temp % 10 + carry);
+                    carry = temp / 10;
+                } else {
+                    result.append(maxNumLen.charAt(i) - '0');
+                }
+            }
+        }
+        return result.reverse().toString();
     }
 }
