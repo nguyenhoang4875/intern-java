9dd163bc3384d68e401695711072be72fd6b5f77
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-04-09 10:22:07 +0700

    exercise-09 v2

--- src/main/java/array/Exer09.java
+++ src/main/java/array/Exer09.java
@@ -3,22 +3,17 @@ package array;
 public class Exer09 {
 
     public int binarySearch(int[] array, int n) {
-        if (array.length == 1) {
-            return array[0] == n ? 0 : -1;
-        }
-        if ((array[0] < array[1]) == (n < array[0] || n > array[array.length - 1])) {
-            return -1;
-        }
-
         int start = 0;
         int end = array.length;
-        int middle = array.length / 2;
+        int middle;
 
-        while (start <= end && (end - start) != 1) {
+        while (end - start > 1) {
+            middle = (start + end) >>> 1;
             if (array[middle] == n) {
                 return middle;
             }
             if (array[1] >= array[0]) {
+
                 if (n > array[middle]) {
                     start = middle;
                 } else {
@@ -31,8 +26,7 @@ public class Exer09 {
                     start = middle;
                 }
             }
-            middle = (start + end) / 2;
         }
-        return -1;
+        return array[0] == n ? 0 : -1;
     }
 }
