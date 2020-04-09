1293ff54cb083695456bc28809c8d8b89584e486
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-04-09 14:35:00 +0700

    exercise-11 v2

--- src/main/java/string/Exer11.java
+++ src/main/java/string/Exer11.java
@@ -2,16 +2,61 @@ package string;
 
 public class Exer11 {
 
-    public int subNumbers(String num1, String num2) {
-        return getNumber(num1) - getNumber(num2);
-    }
-
-    public int getNumber(String number) {
-        int result = 0;
-
-        for (int i = 0; i < number.length(); i++) {
-            result = result * 10 + number.charAt(i) - '0';
+    public String subNumbers(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+        int lenStr1 = num1.length();
+        int lenStr2 = num2.length();
+        String maxNumLen;
+        String minNumLen;
+        boolean greaterThanZero;
+        if ((lenStr1 > lenStr2) || (lenStr1 == lenStr2 && num1.compareTo(num2) >= 0)) {
+            maxNumLen = new StringBuilder(num1).reverse().toString();
+            minNumLen = new StringBuilder(num2).reverse().toString();
+            greaterThanZero = true;
+        } else {
+            maxNumLen = new StringBuilder(num2).reverse().toString();
+            minNumLen = new StringBuilder(num1).reverse().toString();
+            greaterThanZero = false;
         }
-        return result;
+        int temp;
+        int carry = 0;
+
+        for (int i = 0; i < minNumLen.length(); i++) {
+            temp = maxNumLen.charAt(i) - minNumLen.charAt(i) + carry;
+            if (temp < 0) {
+                temp = temp + 10;
+                carry = -1;
+            } else {
+                carry = 0;
+            }
+
+            result.append(temp);
+        }
+        if (maxNumLen.length() > minNumLen.length()) {
+
+            for (int i = minNumLen.length(); i < maxNumLen.length(); i++) {
+                if (carry != 0) {
+                    temp = maxNumLen.charAt(i) + carry - '0';
+                    carry = 0;
+                    if (temp < 0) {
+                        temp = temp + 10;
+                        carry = -1;
+                    }
+                    result.append(temp);
+                } else {
+                    result.append(maxNumLen.charAt(i) - '0');
+                }
+            }
+        }
+
+        for (int i = result.length() - 1; i > 0; i--) {
+            if (result.charAt(i) == '0') {
+                result.replace(i, i + 1, "");
+            } else {
+                break;
+            }
+        }
+        result = greaterThanZero ? result : result.append('-');
+        return result.reverse().toString();
     }
 }
--- src/test/java/string/Exer11Test.java
+++ src/test/java/string/Exer11Test.java
@@ -6,12 +6,17 @@ import org.junit.Test;
 public class Exer11Test {
 
     @Test
-    public void test_sumNumbers(){
+    public void test_sumNumbers() {
+        Assert.assertEquals(new Exer11().subNumbers("2", "1"), "1");
+        Assert.assertEquals(new Exer11().subNumbers("6912", "5678"), "1234");
+        Assert.assertEquals(new Exer11().subNumbers("5678", "6912"), "-1234");
+        Assert.assertEquals(new Exer11().subNumbers("123", "5678"), "-5555");
+        Assert.assertEquals(new Exer11().subNumbers("1000000", "987"), "999013");
+        Assert.assertEquals(new Exer11().subNumbers("987", "1000000"), "-999013");
         Assert.assertEquals(new Exer11().subNumbers(
                 "137817381738163715371357189182918298129829182981",
                 "8178217281786173715325362"),
                 "137817381738163715371349010965636511956113857619");
-
         Assert.assertEquals(new Exer11().subNumbers(
                 "137817381738163715371349010965636511956113857619",
                 "182781271837192381617467246724527542637263263823"),
