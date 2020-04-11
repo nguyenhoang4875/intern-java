2f515a65b16badab41d9bcdf5179779c410eef78
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-04-11 07:38:07 +0700

    exercise-11 v3

--- src/main/java/string/Exer11.java
+++ src/main/java/string/Exer11.java
@@ -4,49 +4,21 @@ public class Exer11 {
 
     public String subNumbers(String num1, String num2) {
         StringBuilder result = new StringBuilder();
-        int lenStr1 = num1.length();
-        int lenStr2 = num2.length();
-        String maxNumLen;
-        String minNumLen;
-        boolean greaterThanZero;
-        if ((lenStr1 > lenStr2) || (lenStr1 == lenStr2 && num1.compareTo(num2) >= 0)) {
-            maxNumLen = new StringBuilder(num1).reverse().toString();
-            minNumLen = new StringBuilder(num2).reverse().toString();
-            greaterThanZero = true;
-        } else {
-            maxNumLen = new StringBuilder(num2).reverse().toString();
-            minNumLen = new StringBuilder(num1).reverse().toString();
-            greaterThanZero = false;
+        if ((num2.length() > num1.length()) || (num2.length() == num1.length() && num2.compareTo(num1) > 0)) {
+            return "-" + subNumbers(num2, num1);
         }
         int temp;
         int carry = 0;
 
-        for (int i = 0; i < minNumLen.length(); i++) {
-            temp = maxNumLen.charAt(i) - minNumLen.charAt(i) + carry;
+        for (int i = 0; i < num1.length(); i++) {
+            temp = digitFromRight(num1, i) - digitFromRight(num2, i) + carry;
             if (temp < 0) {
                 temp = temp + 10;
                 carry = -1;
             } else {
                 carry = 0;
             }
-
             result.append(temp);
-        }
-        if (maxNumLen.length() > minNumLen.length()) {
-
-            for (int i = minNumLen.length(); i < maxNumLen.length(); i++) {
-                if (carry != 0) {
-                    temp = maxNumLen.charAt(i) + carry - '0';
-                    carry = 0;
-                    if (temp < 0) {
-                        temp = temp + 10;
-                        carry = -1;
-                    }
-                    result.append(temp);
-                } else {
-                    result.append(maxNumLen.charAt(i) - '0');
-                }
-            }
         }
 
         for (int i = result.length() - 1; i > 0; i--) {
@@ -56,7 +28,13 @@ public class Exer11 {
                 break;
             }
         }
-        result = greaterThanZero ? result : result.append('-');
         return result.reverse().toString();
+    }
+
+    public int digitFromRight(String number, int index) {
+        if (index >= number.length()) {
+            return 0;
+        }
+        return number.charAt(number.length() - 1 - index) - '0';
     }
 }
