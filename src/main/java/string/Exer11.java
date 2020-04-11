63c8472c00a837df760ec1db9ee639462c600700
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-04-11 14:04:08 +0700

    exercise-11 v4

--- src/main/java/string/Exer11.java
+++ src/main/java/string/Exer11.java
@@ -3,32 +3,29 @@ package string;
 public class Exer11 {
 
     public String subNumbers(String num1, String num2) {
-        StringBuilder result = new StringBuilder();
         if ((num2.length() > num1.length()) || (num2.length() == num1.length() && num2.compareTo(num1) > 0)) {
             return "-" + subNumbers(num2, num1);
         }
-        int temp;
+        StringBuilder result = new StringBuilder();
         int carry = 0;
 
         for (int i = 0; i < num1.length(); i++) {
-            temp = digitFromRight(num1, i) - digitFromRight(num2, i) + carry;
+            int temp = digitFromRight(num1, i) - digitFromRight(num2, i) + carry;
             if (temp < 0) {
                 temp = temp + 10;
                 carry = -1;
             } else {
                 carry = 0;
             }
-            result.append(temp);
+            result.insert(0, temp);
         }
 
-        for (int i = result.length() - 1; i > 0; i--) {
-            if (result.charAt(i) == '0') {
-                result.replace(i, i + 1, "");
-            } else {
-                break;
-            }
+        int first = 0;
+
+        while (first < result.length() && result.charAt(first) == '0') {
+            first++;
         }
-        return result.reverse().toString();
+        return result.substring(first);
     }
 
     public int digitFromRight(String number, int index) {
