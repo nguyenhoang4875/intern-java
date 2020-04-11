c2d6f116f58fb06ccb9f6eadadfa3617df19e0c9
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-04-11 14:09:52 +0700

    exercise-10 v4

--- src/main/java/string/Exer10.java
+++ src/main/java/string/Exer10.java
@@ -5,18 +5,17 @@ public class Exer10 {
     public String sumNumbers(String num1, String num2) {
         StringBuilder result = new StringBuilder();
         int maxLen = num1.length() >= num2.length() ? num1.length() : num2.length();
-        int temp;
         int carry = 0;
 
         for (int i = 0; i < maxLen; i++) {
-            temp = digitFromRight(num1, i) + digitFromRight(num2, i);
-            result.append((temp % 10 + carry) % 10);
+            int temp = digitFromRight(num1, i) + digitFromRight(num2, i);
+            result.insert(0, (temp % 10 + carry) % 10);
             carry = (temp + carry) / 10;
         }
         if (carry > 0) {
-            result.append(carry);
+            result.insert(0, carry);
         }
-        return result.reverse().toString();
+        return result.toString();
     }
 
     public int digitFromRight(String number, int index) {
